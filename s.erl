6a745b987bc05f10b4ba4b6287b507c54d49a2c6
Po Liu <po.liu@hootsuite.com>
2014-06-12 17:34:22 -0700

    sol 2

--- s.erl
+++ s.erl
@@ -3,4 +3,8 @@
 
 last(XS) -> lists:nth(length(XS), XS).
 
-main(_) -> io:format("~w~n", [last([10, 20, 30])]).
+penultimate(XS) -> lists:nth(length(XS)-1, XS).
+
+main(_) ->
+  io:format("~w~n", [last([10, 20, 30])]),
+  io:format("~w~n", [penultimate([10, 20, 30])]).
